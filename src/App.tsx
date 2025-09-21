b92dbac337bead7161412c3ec4eea6b02177fda5
jrrodrigo42 <rjr89000@gmail.com>
2025-09-21 17:10:07 -0300

    fix: jsx in style

--- src/App.tsx
+++ src/App.tsx
@@ -50,7 +50,7 @@ function App() {
       <Footer translations={currentTranslations} />
 
       {/* Custom Styles */}
-      <style jsx>{`
+      <style>{`
         @keyframes fade-in {
           from { opacity: 0; transform: translateY(20px); }
           to { opacity: 1; transform: translateY(0); }
